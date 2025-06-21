007762c707ce624845576de12963e8ca1ce1427f
u2095541 <alpha.bah@warwick.ac.uk>
2025-06-21 21:45:28 +0100

    Add income form handling

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-print ("Hello")
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        income = request.form.get('income')
+        print(f"Income Submitted: {income}")
     return render_template('index.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
